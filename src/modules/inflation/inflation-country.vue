3d95bbcb9d44e81f253f75337a5d1e11e94ad0cc
Everton Buzzi <evertonbuzzi@gmail.com>
2026-04-17 20:42:24 +0100

    eb(feat-3): fix build

--- src/modules/inflation/inflation-country.vue
+++ src/modules/inflation/inflation-country.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed, watch, onMounted, onBeforeUnmount, nextTick } from 'vue'
+import { ref, computed, watch, onMounted, onBeforeUnmount } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 import {
